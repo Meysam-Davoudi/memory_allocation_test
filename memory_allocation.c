8210f1bea70f41ce2ab00cf5c39781bb070cfb74
Meysam Davoudi <36991314+Meysam-Davoudi@users.noreply.github.com>
2024-04-17 17:43:48 +0300

    Initial commit

--- memory_allocation.c
+++ memory_allocation.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+
 #include <stdlib.h>
 
 
-int main(){
+int main() {
 
-	int size = 5;
+    int size = 5;
 
-	//To observe the variance in memory allocation behavior between malloc() and calloc()
+    //To observe the variance in memory allocation behavior between malloc() and calloc()
 
     printf("===== Malloc =====\n");
-    int *malloc_array = (int*) malloc(size * sizeof(int));
+    int * malloc_array = (int * ) malloc(size * sizeof(int));
 
     if (malloc_array == NULL) {
         printf("Memory allocation failed for malloc.\n");
@@ -28,7 +29,7 @@ int main(){
     printf("\n");
 
     printf("===== Calloc =====\n");
-    int *calloc_array = (int*) calloc(size, sizeof(int));
+    int * calloc_array = (int * ) calloc(size, sizeof(int));
 
     if (calloc_array == NULL) {
         printf("Memory allocation failed for calloc.\n");
@@ -42,52 +43,47 @@ int main(){
     }
     printf("\n");
 
-
-
     //Phone-Book
 
-	printf("===== size = %d =====\n", size);
+    printf("===== size = %d =====\n", size);
 
-	int *phonebook = (int*) calloc(size, sizeof(int));
-	//int *phonebook = (int*) malloc(size * sizeof(int));
-	// int *phonebook = (int*) calloc(size, sizeof(int));
+    int * phonebook = (int * ) calloc(size, sizeof(int));
+    //int *phonebook = (int*) malloc(size * sizeof(int));
+    // int *phonebook = (int*) calloc(size, sizeof(int));
 
-	int phone_number = 0;
-	int counter = 0;
+    int phone_number = 0;
+    int counter = 0;
 
-	while(1){
+    while (1) {
 
-		printf("Enter a phone number %d: ", counter);
-		scanf("%d", &phone_number);
-		if (phone_number == -1)
-		{
-			break;
-		}
+        printf("Enter a phone number %d: ", counter);
+        scanf("%d", & phone_number);
+        if (phone_number == -1) {
+            break;
+        }
 
-		if (counter >= size){
+        if (counter >= size) {
 
-			size += 2;
-			printf("===== new size = %d =====\n", size);
+            size += 2;
+            printf("===== new size = %d =====\n", size);
 
-			phonebook = (int*) realloc(phonebook, size * sizeof(int));
-		}
+            phonebook = (int * ) realloc(phonebook, size * sizeof(int));
+        }
 
-		phonebook[counter] = phone_number;
+        phonebook[counter] = phone_number;
 
-		counter++;
-	}
+        counter++;
+    }
 
+    printf("===== List =====\n");
 
+    int i;
+    for (i = 0; i < counter; i++) {
+        printf("number %d : %d \n", i, phonebook[i]);
+    }
 
-	printf("===== List =====\n");
+    free(phonebook);
 
-	int i;
-	for(i=0; i<counter; i++){
-		printf("number %d : %d \n", i, phonebook[i]);
-	}
-
-	free(phonebook);
-
-	return 0;
+    return 0;
 
 }
